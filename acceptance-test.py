84eaaf225479efe13d9a0e6f131d82b4f22e1f4f
Hassan Khalaila <hkh148@gmail.com>
2018-12-17 17:50:18 +0200

    added acc test

---
+++ acceptance-test.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="203761333",
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM AnchorTable WHERE Title = 'רבי עקיבא'")
+entries = mycursor.fetchall()
+
+for entry in entries:
+	print(entry[0])
