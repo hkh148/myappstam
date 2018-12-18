e9024b784b0fac1b469fbd7f090c01009d555974
Hassan Khalaila <hkh148@gmail.com>
2018-12-18 16:04:10 +0200

    blah

---
+++ Spotter.py
@@ -0,0 +1,31 @@
+from nltk import ngrams
+import mysql.connector
+import string
+import sys
+import csv
+
+mydb = mysql.connector.connect(host='localhost', user='root', passwd='203761333',database='projectdb')
+mycursor = mydb.cursor()
+exclude = set(string.punctuation)
+
+my_file = open(sys.argv[1],'r',encoding='utf8')
+result = open(sys.argv[2],'w',encoding='utf8')
+csv_writer = csv.writer(result,dialect='excel',lineterminator='\n')
+data = my_file.read()
+for i in range(1,5):
+    grams = ngrams(data.split(), i)
+	start_index = 0
+    for gram in grams:
+        word = ''.join([w + ' ' for w in gram]).strip()
+        word = ''.join(ch for ch in word if ch not in exclude)
+        if word.find('–') != -1:
+            continue
+        query = "SELECT Link FROM AliasTable" \
+                " WHERE Alias = '" + word + "';"
+        mycursor.execute(query)
+        entries = mycursor.fetchall()
+        for entry in entries:
+			tmp_row = [word,start_index,start_index+i-1,entry[1],entry[0]]
+			csv_writer.write(tmp_row)
+my_file.close()
+result.close()
